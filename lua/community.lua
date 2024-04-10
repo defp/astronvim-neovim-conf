bfe0811695edcdb7f103b4a2e91acc630b67e361
dåshuång li <li.dashuang@icloud.com>
2024-04-10 17:47:22 +0800

    ACTIVATE custom config

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.lua" },
 }

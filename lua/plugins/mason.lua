302cf8a0d18fe3fc661ba6744f0e4e3bb2358d01
dåshuång li <li.dashuang@icloud.com>
2024-11-27 12:36:54 +0800

    Update mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,11 +7,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
+      opts.automatic_installation = false
+      opts.ensure_installed = {}
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
-        -- add more arguments for adding more language servers
-      })
+      -- opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      --   "lua_ls",
+      -- })
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources

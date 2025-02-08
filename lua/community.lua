79b3fc74012c9b32e2a3c1ff13f22fef97046546
dåshuång li <li.dashuang@icloud.com>
2025-02-08 14:55:17 +0800

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -6,7 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.python" },
 }
